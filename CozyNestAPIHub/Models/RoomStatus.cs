6499d32505c65cc4627fdc698d5b71967867fcfc
DevDominik <71924117+DevDominik@users.noreply.github.com>
2025-02-06 11:44:16 +0100

    Delete CozyNestAPIHub directory

--- CozyNestAPIHub/Models/RoomStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CozyNestAPIHub.Models
-{
-    public class RoomStatus
-    {
-        public int Id { get; set; }
-        public string Description { get; set; }
-    }
-}
--- CozyNestAPIHub/Models/RoomType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CozyNestAPIHub.Models
-{
-    public class RoomType
-    {
-        public int Id { get; set; }
-        public string Description { get; set; }
-    }
-}
