cb13eec397e5c0165a2421845fe06e0688692235
kecskes.dominik <kecskes.dominik@BURG-1E06-02>
2025-02-19 09:40:27 +0100

    [+] create endpoint @ RoomController.cs

--- CozyNestAPIHub/Controllers/RoomController.cs
+++ CozyNestAPIHub/Controllers/RoomController.cs
@@ -36,7 +36,71 @@ namespace CozyNestAPIHub.Controllers
                 rooms = final.ToArray()
             });
         }
+        [Route("create")]
+        [HttpPost]
+        [Role("Manager")]
+        public async Task<IActionResult> Create([FromBody] RoomCreateRequest request)
+        {
+            if (request == null |
+                string.IsNullOrEmpty(request.StatusDescription) |
+                string.IsNullOrEmpty(request.TypeDescription) |
+                string.IsNullOrEmpty(request.RoomNumber) |
+                request.PricePerNight == null |
+                request.Description == null)
+            {
+                return BadRequest(new
+                {
+                    message = "Invalid request."
+                });
+            }
+            RoomStatus? roomStatus = await RoomHandler.GetRoomStatusByDescription(request.StatusDescription);
+            if (roomStatus == null)
+            {
+                return BadRequest(new
+                {
+                    message = "Invalid room status."
+                });
+            }
+            RoomType? roomType = await RoomHandler.GetRoomTypeByDescription(request.TypeDescription);
+            if (roomType == null)
+            {
+                return BadRequest(new
+                {
+                    message = "Invalid room type."
+                });
+            }
+            Room room = new Room()
+            {
+                Deleted = false,
+                Description = request.Description,
+                RoomNumber = request.RoomNumber,
+                Status = roomStatus.Id,
+                Type = roomType.Id,
+                PricePerNight = request.PricePerNight,
+            };
 
+            Room? createdRoom = await RoomHandler.CreateRoom(room);
+            if (createdRoom == null) 
+            {
+                return StatusCode(500, new
+                {
+                    message = "Error arose when creating room."
+                });
+            }
+            return Ok(new
+            {
+                message = "New room created.",
+                roomData = new {
+                    id = createdRoom.Id,
+                    description = createdRoom.Description,
+                    status = roomStatus.Description,
+                    type = roomType.Description,
+                    pricePerNight = createdRoom.PricePerNight,
+                    deleted = createdRoom.Deleted,
+                    roomNumber = createdRoom.RoomNumber,
+                }
+            });
+        }
         
     }
 }
