6d92eb248e8308e878bd32fb54993863d8b7d441
kecskes.dominik <kecskes.dominik@BURG-1E06-02>
2025-02-19 13:48:22 +0100

    asd

--- CozyNestAPIHub/RequestTypes/UserSelfUpdateRequest.cs
+++ CozyNestAPIHub/RequestTypes/UserSelfUpdateRequest.cs
@@ -3,7 +3,7 @@
     public class UserSelfUpdateRequest
     {
         public string Username { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
